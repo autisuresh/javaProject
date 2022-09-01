790f240ba8d24ed778691545224cd444efe5d2b4
autisure <auti.suresh@gmail.com>
2022-09-02 01:35:26 +0530

    first commit java project

---
+++ src/main/java/com/auti/nirmala/nirmalaproject/NirmalaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.auti.nirmala.nirmalaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirmalaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirmalaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auti/nirmala/nirmalaproject/NirmalaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auti.nirmala.nirmalaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirmalaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
